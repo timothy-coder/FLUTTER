ec25660ebb8b743e728f73c848688d6945bd7597
timothy-coder <nicopar666xd@gmail.com>
2023-12-14 10:56:13 -0500

    as

--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project_app/app.dart';
+import 'package:flutter_project_app/utils/constants/sizes.dart';
+import 'package:get/get.dart';
 
 class ResetPassword extends StatelessWidget {
   const ResetPassword({super.key});
@@ -6,6 +10,22 @@ class ResetPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: ()=>Get.back(), icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              ///image with a 60% screen
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
