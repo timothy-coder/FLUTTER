5eb32a857dc716825b4f3da04039d25f785d1ec3
timothy-coder <78666231+timothy-coder@users.noreply.github.com>
2023-12-17 16:42:13 -0500

    as

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:flutter_project_app/utils/constants/sizes.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Container()
+              child: Column(
+                children: [
+                  ///AppBar
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///SearchBar
+                  TSearchContainer(text: 'Search in Vademecum',),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///Categorias
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [],
+                    ),
+                  ),
+                ],
+              )
             ),
           ],
         ),
@@ -18,3 +39,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
