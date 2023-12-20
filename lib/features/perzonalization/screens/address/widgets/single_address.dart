97b57c7af869478e5d76d2fdeba6a8b87a76b3fc
timothy-coder <nicopar666xd@gmail.com>
2023-12-20 15:28:45 -0500

    Update single_address.dart

--- lib/features/perzonalization/screens/address/widgets/single_address.dart
+++ lib/features/perzonalization/screens/address/widgets/single_address.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_app/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:flutter_project_app/utils/constants/colors.dart';
+import 'package:flutter_project_app/utils/constants/sizes.dart';
 import 'package:flutter_project_app/utils/helpers/helper_functions.dart';
+import 'package:iconsax/iconsax.dart';
 class TSingleAddress extends StatelessWidget {
   const TSingleAddress({
     super.key,
@@ -16,6 +18,24 @@ class TSingleAddress extends StatelessWidget {
       width: double.infinity,
       showBorder: true,
       backgroundColor: selectedAddress ? TColors.primary.withOpacity(0.5) : Colors.transparent,
+      borderColor: selectedAddress
+          ? Colors.transparent
+          : dark
+            ? TColors.darkerGrey
+            : TColors.grey,
+      margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+      child: Stack(
+        children: [
+          Icon(
+            selectedAddress ? Iconsax.tick_circle5 : null,
+            color: selectedAddress
+                ? dark
+                  ? TColors.light
+                  : TColors.dark
+                :null,
+          ),
+        ],
+      ),
     );
   }
 }
